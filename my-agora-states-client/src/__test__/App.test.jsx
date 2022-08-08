34cbc7bc789304b5726f070a966a7918c29bcfbd
MoSangBin <mosangbin94@gmail.com>
2022-08-08 15:00:54 +0900

    test

--- my-agora-states-client/src/__test__/App.test.jsx
+++ my-agora-states-client/src/__test__/App.test.jsx
@@ -1,14 +1,14 @@
 // App 컴포넌트가 렌더링 되면 'My Agora States'가 존재한다.
 
-import "@testing-library/jest-dom";
+// import "@testing-library/jest-dom";
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+require('@testing-library/jest-dom');
 
 test("App 컴포넌트가 렌더링 되면 'My Agora States'가 존재한다.", () => {
-  // render(<App />);
+  render(<App />);
 
-  // const title = screen.getByRole('heading')
-  // // const title = screen.getByText('My Agora States')
-  // expect(title).toHaveTextContent('My Agora States');
+  const title = screen.getByRole('heading')
+  expect(title).toHaveTextContent('My Agora States');
   expect(true).toBeTruthy();
 })
